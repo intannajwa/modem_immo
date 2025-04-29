17704b5f148d0073fd0d18af3ace67148e94d28e
Intan Najwa binti Ahmad <126443940+intannajwa@users.noreply.github.com>
2025-04-29 19:43:34 +0800

    Update nfttl.lua

--- luci-app-nfttl/luasrc/controller/nfttl.lua
+++ luci-app-nfttl/luasrc/controller/nfttl.lua
@@ -1,5 +1,10 @@
 module("luci.controller.nfttl", package.seeall)
 
 function index()
-    entry({"admin", "network", "nfttl"}, cbi("ttl"), _("TTL Configuration"), 90)
+    -- Ensure "Modem" menu exists
+    local page = entry({"admin", "modem"}, firstchild(), _("Modem"), 60)
+    page.dependent = false
+
+    -- Add under Modem menu
+    entry({"admin", "modem", "nfttl"}, cbi("nfttl/ttl"), _("TTL Settings"), 50).leaf = true
 end
